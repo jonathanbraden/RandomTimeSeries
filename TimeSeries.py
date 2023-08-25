3b512d8a9701aa3249560c23ec9a1df51b54d03a
Jonathan Braden <jonathan.n.braden@gmail.com>
2023-08-25 13:31:42 -0400

    Improving classes for generating ARMA, AR, MA time streams

--- TimeSeries.py
+++ TimeSeries.py
@@ -5,10 +5,27 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-# Add option to start the series with random noise
-class TimeSeries_AR():
+# To Do:
+#   - Add option to start time-streams from some mean value, or sampled from some i.i.d. distribution
+#   - Add some more convenience routines for computing lags, etc.
+#   - Add some caching of previously generated time series
+#   - Add option to specify the i.i.d. distribution samples are drawn from
+class TimeSeries_AR:
+    """
+    A class to generate time-stream from AR(p) models.
+    """
+    
+    def __init__(self, ar_coeffs, phi0 : float):
+        """
+        Initialise an AR(p) time-series model 
+          y_t = phi_0 + \sum_{l=1}^q w_l y_{t-q} + eta_t
 
-    def __init__(self, ar_coeffs=[0.3,0.7], phi0=0.3):
+        Inputs
+        ------
+          ar_coeffs : The coefficients w_l above
+          phi0      : The coefficeint phi0 above
+        """
+        # Add some data checks here
         self.noise = np.random.normal
 
         self.ar_coeffs = np.array(ar_coeffs)
@@ -17,17 +34,26 @@ class TimeSeries_AR():
 
         self.is_stationary = np.sum(ar_coeffs) < 1. # Check this
 
-    # Add an option for how to start the samples here
-    # Add an option for burn-in here
-    def sample(self,num_samples, num_steps, *, mean_start=0., burn_in=0):
+    # Add an option to either start the series with some mean value
+    # or else drawing from a distribution
+    def sample(self,num_samples, num_steps, *, burn_in=0):
         p_ = self.ar_ord
         
         noise = self.noise( size=(num_samples, num_steps+burn_in) )
         series = np.zeros( (num_samples, num_steps+p_+burn_in) )
-        series[:,:p_] = mean_start
-        
+
+        if self.is_stationary:
+            series[:,:p_] = self.mean()
+        else:
+            series[:,:p_] = 0.
+            
+            
         for i in range(num_steps+burn_in): 
-            series[:,i+p_] = np.sum(series[:,i:i+p_]*self.ar_coeffs) + noise[:,i]
+            series[:,i+p_] = ( self.phi0 +
+                               np.sum(series[:,i:i+p_]*self.ar_coeffs,axis=-1)
+                               + noise[:,i]
+                               )
+            
         return series[:,p_+burn_in:] 
 
     def mean(self):
@@ -37,12 +63,68 @@ class TimeSeries_AR():
         Otherwise return NaN
         """
         if (self.is_stationary):
-            return self.phi0 / (1.-np.sum(ar_coeffs))
+            return self.phi0 / (1.-np.sum(self.ar_coeffs))
         return np.nan
+    
+    
+class TimeSeries_MA:
 
-class TimeSeries_MA():
+    def __init__(self, ma_coeffs, mu):
+        self.ma_coeffs = np.array(ma_coeffs)
+        self.mu = mu
+        self.ma_ord = ma_coeffs.size
+        
+        return
 
-    def __init__(self, ma_ord, ma_coeffs, ):
+    def sample(self, num_samples: int, num_steps: int, *, mean_start=0.):
+        q_ = self.ma_ord
+
+        noise = self.noise( size=(num_samples, num_steps+q_) )
+        series = np.zeros( (num_samples, num_steps+q_) )
+        for i in range(num_steps):
+            series[:,i+q_] = mu + np.sum(series[:,i:i+q_]*self.ma_coeffs) + noise[:,i]
+
+        return series
+
+    def mean(self):
+        return self.mu
+
+class TimeSeries_ARMA:
+
+    def __init__(self, ma_coeffs, ar_coeffs, phi0: float):
+        self.noise = np.random.normal
+        
+        self.ma_coeffs = np.array(ma_coeffs)
+        self.ar_coeffs = np.array(ar_coeffs)
+        
+        self.ma_ord = self.ma_coeffs.size
+        self.ar_ord = self.ar_coeffs.size
+        self.phi0 = phi0
+
+        return
+
+    def sample(self, num_samples: int, num_steps: int, *, burn_in : int = 0):
+        p_, q_ = self.ar_ord, self.ma_ord
+        pad = max(p_,q_)
+        
+        noise = self.noise( size=(num_samples, num_steps+q_+burn_in) )
+        series = np.zeros( (num_samples, num_steps+pad+burn_in) )
+
+        series[:,:pad] = self.mean()  # Adjust this
+        
+        for i in range(num_steps+burn_in):
+            series[:,i+pad] = ( self.phi0 +
+                                np.sum(self.ar_coeffs*series[:,i+pad-p_:i+pad],axis=-1) +
+                                np.sum(self.ma_coeffs*noise[:,i+pad-q_:i+pad],axis=-1) +
+                                noise[:,i]
+                                )
+        
+        return series[:,pad+burn_in:]
+
+    def mean(self):
+        return self.phi0 / (1.-np.sum(self.ar_coeffs))
+
     
 if __name__=="__main__":
-    pass
+    ARMA = TimeSeries_ARMA([0.3,0.4, 0.5], [0.1,0.3], 1.7)
+    s = ARMA.sample(10000,256)
